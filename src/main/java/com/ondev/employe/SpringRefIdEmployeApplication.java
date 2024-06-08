85a779f0383a6236097551ca1a96bc42c709f8c0
jean741 <iattiglah@gmail.com>
2024-06-08 21:58:13 +0200

    init

---
+++ src/main/java/com/ondev/employe/SpringRefIdEmployeApplication.java
@@ -0,0 +1,13 @@
+package com.ondev.employe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRefIdEmployeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRefIdEmployeApplication.class, args);
+    }
+
+}
